2be02387a4fd7aa9bf654861c1ef67552b9a2a5e
DebbieZimmerman <debbieseuss@gmail.com>
2022-09-01 01:02:36 +0300

    basic popular done

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,4 +1,7 @@
 import { useEffect, useState } from "react"
+import styled from "styled-components"
+import { Splide, SplideSlide } from '@splidejs/react-splide'
+import "@splidejs/splide/dist/css/splide.min.css"
 
 function Popular() {
 
@@ -16,13 +19,49 @@ function Popular() {
 
   return (
     <div>
-      {popular.map(recipe => {
-        return(
+         <Wrapper>
+            <h3>Popular Recipes</h3>
 
-        )
-      })}
+            <Splide options={{
+              perPage:4,
+              arrows: false,
+              pagination: false,
+              drag: "free",
+              gap: "5rem"
+            }}>
+            {popular.map((recipe) =>{
+              return(
+                <SplideSlide>
+                <Card>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt ={recipe.title} />
+                </Card>
+                </SplideSlide>
+              )
+            })}
+            </Splide>
+          </Wrapper>   
     </div>
   )
 }
+
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+  `
+const Card = styled.div`
+  min-height: 25rem;
+  border-radius: 2rem;
+  overflow: hidden;
+  position: relative;
+
+  img{
+    border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
 
 export default Popular
